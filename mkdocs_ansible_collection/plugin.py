1d21d2570e825017bc92934a9fdd8d5e3d6d8d58
Cristian Sirbu <cmsirbu@users.noreply.github.com>
2024-07-29 20:49:28 +0100

    Refactor to use jinja templates and add mkdocs logger

--- mkdocs_ansible_collection/plugin.py
+++ mkdocs_ansible_collection/plugin.py
@@ -2,23 +2,39 @@ import mkdocs
 import subprocess
 import json
 
+from importlib.resources import files as package_files
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from mkdocs.exceptions import PluginError
+from mkdocs.plugins import get_plugin_logger
+
+# Warning level messages break the build if --strict is passed!
+# Debug level messages show only if --verbose is passed!
+log = get_plugin_logger(__name__)
+
 
 class AnsibleDocsPluginConfig(mkdocs.config.base.Config):
-    plugins = mkdocs.config.config_options.Type(list, default=[])
+    # plugins = mkdocs.config.config_options.Type(list, default=[])
     collections = mkdocs.config.config_options.Type(list, default=[])
 
 
-# TODO: Use mkdocs logger
 class AnsibleDocsPlugin(mkdocs.plugins.BasePlugin[AnsibleDocsPluginConfig]):
     def on_pre_build(self, config, **kwargs):
-        if self.config.plugins:
-            print(f"Plugins list: {self.config.plugins}")
+        # if self.config.plugins:
+        #     log.debug(f"Plugins list: {self.config.plugins}")
         if self.config.collections:
-            print(f"Collections list: {self.config.collections}")
-
-        # print(config)
+            log.debug(f"Collections list: {self.config.collections}")
 
     def on_files(self, files, config):
+        # Load templates from package and initialize Jinja environment
+        log.debug(
+            f"Jinja templates path {package_files('mkdocs_ansible_collection') / 'templates'}"
+        )
+        jinja_env = Environment(
+            loader=FileSystemLoader(package_files("mkdocs_ansible_collection") / "templates"),
+            autoescape=select_autoescape(default=True),
+            trim_blocks=True,
+        )
+
         for fqcn in self.config.collections:
             # Get collection metadata by running ansible-doc
             collection_metadata = AnsibleDocsPlugin._get_ansible_doc_metadata(fqcn)
@@ -28,7 +44,12 @@ class AnsibleDocsPlugin(mkdocs.plugins.BasePlugin[AnsibleDocsPluginConfig]):
                 f"{fqcn}/index.md", src_dir=None, dest_dir=config.site_dir, use_directory_urls=False
             )
             nf.generated_by = "ansible-docs"
-            nf.content_string = f"# Collection: {fqcn}"
+
+            jinja_template = jinja_env.get_template("collection_index.md.jinja")
+            nf.content_string = jinja_template.render(
+                plugin_types=collection_metadata["all"], fqcn=fqcn
+            )
+
             files.append(nf)
             collection_nav = {f"{fqcn}": [f"{fqcn}/index.md"]}
 
@@ -44,8 +65,9 @@ class AnsibleDocsPlugin(mkdocs.plugins.BasePlugin[AnsibleDocsPluginConfig]):
                     use_directory_urls=False,
                 )
                 nf.generated_by = "ansible-docs"
-                plugin_markdown_list = "\n".join("- " + plugin for plugin in plugins)
-                nf.content_string = f"# {plugin_type}\n\n{plugin_markdown_list}"
+
+                jinja_template = jinja_env.get_template("plugin_list.md.jinja")
+                nf.content_string = jinja_template.render(plugin_type=plugin_type, plugins=plugins)
 
                 files.append(nf)
                 collection_nav[fqcn].append({f"{plugin_type}": f"{fqcn}/{plugin_type}.md"})
@@ -54,18 +76,26 @@ class AnsibleDocsPlugin(mkdocs.plugins.BasePlugin[AnsibleDocsPluginConfig]):
 
         return files
 
+    # Comment this when code is stable, used only for debugging
     def on_nav(self, nav, config, files):
-        pass
-        print(config.nav)
-        print(nav.items)
-
+        log.debug(f"config.nav = {config.nav}")
         # breakpoint()
 
     @staticmethod
     def _get_ansible_doc_metadata(fqcn):
-        # TODO: Add error handling
+        log.info(f"Fetching collection {fqcn} metadata from ansible-doc.")
         result = subprocess.run(
             ["ansible-doc", "--metadata-dump", "--no-fail-on-errors", fqcn], capture_output=True
         )
-        # result.returncode | stderr
-        return json.loads(result.stdout)
+        if result.returncode != 0:
+            command = " ".join(["ansible-doc", "--metadata-dump", "--no-fail-on-errors", fqcn])
+            log.error(f"Command {command} failed with stderr: {result.stderr}")
+            raise PluginError(
+                f"Couldn't fetch collection {fqcn} metadata due to errors from ansible-doc!"
+            )
+        else:
+            try:
+                parsed_data = json.loads(result.stdout)
+            except json.decoder.JSONDecodeError:
+                raise PluginError("Couldn't parse ansible-doc output as valid JSON data!")
+            return parsed_data
