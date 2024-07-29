b09174474585b602d3bd75ae612f2873f392f56a
Cristian Sirbu <cmsirbu@users.noreply.github.com>
2024-07-29 10:12:08 +0100

    Ruff it up

--- mkdocs_ansible_collection/plugin.py
+++ mkdocs_ansible_collection/plugin.py
@@ -2,9 +2,11 @@ import mkdocs
 import subprocess
 import json
 
+
 class AnsibleDocsPluginConfig(mkdocs.config.base.Config):
     plugins = mkdocs.config.config_options.Type(list, default=[])
     collections = mkdocs.config.config_options.Type(list, default=[])
+
 
 # TODO: Use mkdocs logger
 class AnsibleDocsPlugin(mkdocs.plugins.BasePlugin[AnsibleDocsPluginConfig]):
@@ -14,34 +16,35 @@ class AnsibleDocsPlugin(mkdocs.plugins.BasePlugin[AnsibleDocsPluginConfig]):
         if self.config.collections:
             print(f"Collections list: {self.config.collections}")
 
-        #print(config)
-
+        # print(config)
 
     def on_files(self, files, config):
-
         for fqcn in self.config.collections:
             # Get collection metadata by running ansible-doc
             collection_metadata = AnsibleDocsPlugin._get_ansible_doc_metadata(fqcn)
 
             # Generate the index for the collection sub-path
-            nf = mkdocs.structure.files.File(f"{fqcn}/index.md", src_dir=None, dest_dir=config.site_dir, use_directory_urls=False)
+            nf = mkdocs.structure.files.File(
+                f"{fqcn}/index.md", src_dir=None, dest_dir=config.site_dir, use_directory_urls=False
+            )
             nf.generated_by = "ansible-docs"
             nf.content_string = f"# Collection: {fqcn}"
             files.append(nf)
-            collection_nav = {
-                f"{fqcn}": [
-                    f"{fqcn}/index.md"
-                ]
-            }
+            collection_nav = {f"{fqcn}": [f"{fqcn}/index.md"]}
 
             for plugin_type in collection_metadata["all"]:
-                plugins = collection_metadata['all'][plugin_type]
+                plugins = collection_metadata["all"][plugin_type]
                 if len(plugins) == 0:
                     continue
 
-                nf = mkdocs.structure.files.File(f"{fqcn}/{plugin_type}.md", src_dir=None, dest_dir=config.site_dir, use_directory_urls=False)
+                nf = mkdocs.structure.files.File(
+                    f"{fqcn}/{plugin_type}.md",
+                    src_dir=None,
+                    dest_dir=config.site_dir,
+                    use_directory_urls=False,
+                )
                 nf.generated_by = "ansible-docs"
-                plugin_markdown_list = '\n'.join('- ' + plugin for plugin in plugins)
+                plugin_markdown_list = "\n".join("- " + plugin for plugin in plugins)
                 nf.content_string = f"# {plugin_type}\n\n{plugin_markdown_list}"
 
                 files.append(nf)
@@ -56,11 +59,13 @@ class AnsibleDocsPlugin(mkdocs.plugins.BasePlugin[AnsibleDocsPluginConfig]):
         print(config.nav)
         print(nav.items)
 
-        #breakpoint()
+        # breakpoint()
 
     @staticmethod
     def _get_ansible_doc_metadata(fqcn):
         # TODO: Add error handling
-        result = subprocess.run(["ansible-doc", "--metadata-dump", "--no-fail-on-errors", fqcn], capture_output=True)
+        result = subprocess.run(
+            ["ansible-doc", "--metadata-dump", "--no-fail-on-errors", fqcn], capture_output=True
+        )
         # result.returncode | stderr
         return json.loads(result.stdout)
